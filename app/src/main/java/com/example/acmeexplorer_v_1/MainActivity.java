41c9a8fe1a5203da2935facee5380d76c1a26c30
cargonare <cargonare1@alum.us.es>
2023-05-02 10:05:40 +0200

    Firebase al fin va

--- app/src/main/java/com/example/acmeexplorer_v_1/MainActivity.java
+++ app/src/main/java/com/example/acmeexplorer_v_1/MainActivity.java
@@ -1,17 +1,23 @@
 package com.example.acmeexplorer_v_1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.ListView;
 
 import com.example.acmeexplorer_v_1.adapters.EnlaceAdapter;
 import com.example.acmeexplorer_v_1.models.Enlace;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.FirebaseApp;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
     private ArrayList<Enlace> enlace;
+    private BottomNavigationView bottomNav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +29,21 @@ public class MainActivity extends AppCompatActivity {
         EnlaceAdapter adapter = new EnlaceAdapter(enlace, this);
 
         ListView lvMainMenu = findViewById(R.id.lvMainMenu);
+        bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(this);
 
         lvMainMenu.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull android.view.MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.page_profile:
+                Intent intent = new Intent(this, LoginActivity.class);
+                startActivity(intent);
+                return true;
+        }
+
+        return false;
     }
 }
